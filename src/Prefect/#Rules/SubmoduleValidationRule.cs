3a5e240702d60453e5ce21a074d3b02b1491bb11
David Maas <d.maas+git@neurogears.org>
2025-06-21 06:47:34 -0500

    Improved robustness of submodule validation.
    Fixes https://github.com/bonsai-rx/prefect/issues/9

--- src/Prefect/#Rules/SubmoduleValidationRule.cs
+++ src/Prefect/#Rules/SubmoduleValidationRule.cs
@@ -13,16 +13,27 @@ internal sealed class SubmoduleValidationRule : Rule
     private struct Submodule
     {
         public string Name;
-        public string Path; //TODO: P sure this is optional
+        public string Path;
         public string Url;
     }
+
+    /// <summary>Path => Repo Info pairs which must be present in the .gitmodules</summary>
+    private List<(string repoSlug, string repoPath, string? expectedHeadRevision)> requiredSubmodules =
+    [
+        ("bonsai-rx/docfx-tools", "docs/bonsai-docfx", "5b584cadc5f1e3088f4972469243df2e3faf6925"),
+    ];
+
+    /// <summary>List of repo slugs which must not be present in the .gitmodules</summary>
+    private List<string> forbiddenSubmodules =
+    [
+        // Legacy docfx assets repo
+        "bonsai-rx/docfx-assets",
+    ];
 
     public override string? Validate(Repo repo)
     {
         string relativePath = ".gitmodules";
         string gitmodulesPath = Path.Combine(repo.RootPath, relativePath);
-        if (!File.Exists(gitmodulesPath))
-            return null;
 
         StringBuilder errors = new();
 
@@ -30,6 +41,7 @@ internal sealed class SubmoduleValidationRule : Rule
         // Probably not the most robust parsing, but it'll get the job done
         // (This file is almost always manipulated via Git and not directly, so it should be consistent.)
         List<Submodule> submodules = new();
+        if (File.Exists(gitmodulesPath))
         {
             int lineNumber = 0;
             string? name = null;
@@ -110,7 +122,53 @@ internal sealed class SubmoduleValidationRule : Rule
                 errors.AppendLine($"Finished reading submodules file but we never finished parsing the last entry!");
         }
 
+        // Validate submodules
+        HashSet<string> missingSubmodules = new(requiredSubmodules.Select(s => s.repoSlug));
+
         foreach (Submodule submodule in submodules)
+        {
+            // Check if this submodule is forbidden
+            bool isForbidden = forbiddenSubmodules.Any(forbidden => submodule.Url.Contains(forbidden));
+            if (isForbidden)
+            {
+                errors.AppendLine($"Git submodule '{submodule.Name}' at '{submodule.Path}' is not permitted.");
+                break;
+            }
+
+            // Check if this submodule is required
+            foreach ((string repoSlug, string expectedPath, string? expectedHeadRevision) in requiredSubmodules)
+            {
+                if (!submodule.Url.Contains(repoSlug))
+                    continue;
+
+                missingSubmodules.Remove(repoSlug);
+
+                // Validate location
+                if (submodule.Path != expectedPath)
+                    errors.AppendLine($"Git submodule '{submodule.Name}' at '{submodule.Path}' is epxected to be at '{expectedPath}'");
+
+                // Validate revision
+                if (expectedHeadRevision is not null && GetSubmoduleRevision(submodule) is string actualHeadRevision && expectedHeadRevision != actualHeadRevision)
+                    errors.AppendLine($"Git submodule '{submodule.Name}' points to Git revision '{actualHeadRevision}' rather than the expected '{expectedHeadRevision}' (submodule is likely out-of-date)");
+
+                break;
+            }
+        }
+
+        // Complain about all missing required submodules
+        foreach (string repoSlug in missingSubmodules)
+        {
+            (_, string repoPath, string? expectedHeadRevision) = requiredSubmodules.First(s => s.repoSlug == repoSlug);
+
+            errors.Append($"Expected to find submodule at '{repoPath}' pointed to {repoSlug}");
+            if (expectedHeadRevision is not null)
+                errors.Append($" @ {expectedHeadRevision}");
+            errors.AppendLine();
+        }
+
+        return errors.Length > 0 ? errors.ToString() : null;
+
+        string? GetSubmoduleRevision(Submodule submodule)
         {
             string relativeGitFolder = Path.Combine(".git", "modules", submodule.Name);
             string gitFolder = Path.Combine(repo.RootPath, relativeGitFolder);
@@ -120,7 +178,7 @@ internal sealed class SubmoduleValidationRule : Rule
             if (!File.Exists(headFilePath) || !File.Exists(submoduleGitPointerPath))
             {
                 errors.AppendLine($"Git submodule '{submodule.Name}' @ '{submodule.Path}' does not appear to be checked out, ensure submodules are up-to-date.");
-                continue;
+                return null;
             }
 
             string headRevision = File.ReadAllText(headFilePath).Trim();
@@ -133,26 +191,13 @@ internal sealed class SubmoduleValidationRule : Rule
                 if (!File.Exists(namedRefFilePath))
                 {
                     errors.AppendLine($"Could not find named ref for '{submodule.Name}' @ '{submodule.Path}'");
-                    continue;
+                    return null;
                 }
 
                 headRevision = File.ReadAllText(namedRefFilePath).Trim();
             }
 
-            if (submodule.Url.Contains("bonsai-rx/docfx-tools"))
-            {
-                if (headRevision != "5b584cadc5f1e3088f4972469243df2e3faf6925")
-                    errors.AppendLine("docfx-tools is out of date.");
-
-                const string modernDocFxToolsLocaiton = "docs/bonsai-docfx";
-                if (submodule.Path != modernDocFxToolsLocaiton)
-                    errors.AppendLine($"docfx-tools should be located at '{modernDocFxToolsLocaiton}'");
-            }
-
-            if (submodule.Url.Contains("bonsai-rx/docfx-assets"))
-                errors.AppendLine($"Git submodule '{submodule.Name}' at '{submodule.Path}' is the deprecated docfx assets repo.");
+            return headRevision;
         }
-
-        return errors.Length > 0 ? errors.ToString() : null;
     }
 }
